730294f0730b3c3336c0e9a82a875c9a80917097
Ryan Asensio <ryan.asensio@gmail.com>
2015-08-17 22:37:18 -0600

    added 5 guess extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,39 +1,48 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
+@@guesses = 0
 
 def render_me(number, message)
-  erb :index, :locals => {:number => number, :message => message}
+  erb :index, :locals => {:number => number,
+                          :message => message,
+                          :guesses => @@guesses}
 end
 
-def check_guess(guess, number)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess)
     case
-      when guess.to_i > (number + 5)
+      when guess.to_i > (SECRET_NUMBER + 5)
         message = "Way Too high!"
         render_me('??', message)
-      when guess.to_i > number 
+      when guess.to_i > SECRET_NUMBER 
         message = "Too high!"
         render_me('??', message)
-      when guess.to_i < (number - 5)
+      when guess.to_i < (SECRET_NUMBER - 5)
         message = "Way Too low!"
         render_me('??', message)
-      when guess.to_i < number
+      when guess.to_i < SECRET_NUMBER
         message = "Too low!"
         render_me('??', message)
-      when guess.to_i == number
+      when guess.to_i == SECRET_NUMBER
         message = "You got it!"
-        render_me(number, message)
+        render_me(SECRET_NUMBER, message)
     end
 end
 
-number = rand(100)
-
 get '/' do
-  render_me('??', '')
-  guess =  params["guess"]
-  check_guess(guess, number)
+  @@guesses -= 1
+  if @@guesses < 1
+    last_num = SECRET_NUMBER  
+    SECRET_NUMBER = rand(100) 
+    @@guesses = 5
+    render_me(last_num, "Out of guesses, here's a new one")
+  else 
+    render_me('??', '')
+    guess =  params["guess"]
+    check_guess(guess)
+  end
 end
-
-
-
 
